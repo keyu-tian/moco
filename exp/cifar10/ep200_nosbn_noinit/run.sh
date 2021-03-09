98b76fce937851b3629eba6e5b36b8e0b57d7df5
tky_hm_xps13h <k_code@163.com>
2021-03-09 20:40:06 +0800

    not init_params!

--- exp/cifar10/ep200_nosbn_noinit/run.sh
+++ exp/cifar10/ep200_nosbn_noinit/run.sh
@@ -45,7 +45,9 @@ else
   touch "${EXP_DIR}".terminate
 fi
 
+# with init:
 # pretrain exp-2021-0309-120416-VI_SP_VA_1080TI:
 #  mean-top accs @ (min=83.829, mean=84.087, std=0.242) tensor([84.4080, 84.0120, 84.1000, 83.8290]))
 #  best     accs @ (min=83.920, mean=84.162, std=0.236) tensor([84.4800, 84.0800, 84.1700, 83.9200]))
 
+# without init:
