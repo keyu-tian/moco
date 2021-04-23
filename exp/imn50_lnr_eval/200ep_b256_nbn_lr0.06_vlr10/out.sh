b3c685d9717b39d1faf7160ca7826e84f6b7772e
tky_hm_xps13h <k_code@163.com>
2021-04-24 05:50:20 +0800

    seatable log proc=-1 when exception raised

--- exp/imn50_lnr_eval/200ep_b256_nbn_lr0.06_vlr10/out.sh
+++ exp/imn50_lnr_eval/200ep_b256_nbn_lr0.06_vlr10/out.sh
@@ -7,8 +7,8 @@ EXP_DIR="exp-$(date "+%Y-%m%d-%H%M%S")"
 python "${REL_PATH}monitor.py" "${EXP_DIR}" &
 
 PYTHONPATH=${PYTHONPATH}:${REL_PATH} GLOG_vmodule=MemcachedClient=-1 \
-spring.submit run --gpu -n2 \
---ntasks-per-node=2 \
+spring.submit run --gpu -n4 \
+--ntasks-per-node=4 \
 --cpus-per-task=6 \
 --job-name "${DIR_NAME}----${EXP_DIR}" "python -u -m main --main_py_rel_path=${REL_PATH} --exp_dirname=${EXP_DIR} --cfg=cfg.yaml"
 
