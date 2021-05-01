16acaea2a9b4bb19e7e8d4da3514b75c79197996
tky_hm_xps13h <k_code@163.com>
2021-05-01 21:52:21 +0800

    mom 0.3

--- main.py
+++ main.py
@@ -766,7 +766,7 @@ def train_one_ep(is_pretrain, prefix, lg, g_tb_lg, l_tb_lg, dist, meta: ExpMeta,
                 g_tb_lg.add_scalar(f'{prefix}/orig_norm', orig_norm, cur_iter)
                 g_tb_lg.add_scalars(f'{prefix}/lr', {'actual': actual_lr}, cur_iter)
         
-        if tr_loss_avg.last > 12 or not is_pretrain and epoch < 2:
+        if tr_loss_avg.last > 12 or (not is_pretrain and epoch < 2 and cur_iter % 4 == 0):
             l_tb_lg.add_scalar(f'{prefix}/train_loss', tr_loss_avg.last, cur_iter)
         
         if using_auto_aug:
